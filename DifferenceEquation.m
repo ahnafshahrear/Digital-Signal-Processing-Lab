8e3da36d4d638c7efff9c829a1af86eec6ef8147
Ahnaf Shahrear Khan <ahnafshahrearkhan@gmail.com>
2022-11-15 19:05:12 +0600

    Difference Equation added

--- DifferenceEquation.m
+++ DifferenceEquation.m
@@ -12,5 +12,34 @@ stem(n,x,".");
 title("Unit sample sequence");
 axis tight;
 
+#... With filter function
 b = [5 0 -4 1]; #... Coefficients of x
-a = [1 -2] #... Coefficients of y
+a = [2 -5] #... Coefficients of y
+#... 2*y(n)-5*y(n-1) = 5*x(n)-4*x(n-2)+1*x(n-3)
+y = filter(b,a,x);
+subplot(2,2,2);
+stem(n,y,".");
+title("y(n) with filter function");
+axis tight;
+
+#... Without filter function
+yPrime = zeros(1,length(n));
+for i = 1:length(n)
+  if(n(i)<0)
+    yPrime(i) = 0;
+  endif
+  if(n(i)>=0)
+    yPrime(i) = 5*yPrime(i-1)+5*x(i)-4*x(i-2)+x(i-3);
+    yPrime(i) = abs(yPrime(i)/2);
+  endif
+endfor
+subplot(2,2,3);
+stem(n,yPrime,".");
+title("y(n) without filter function");
+axis tight;
+
+#... Normalization
+maxValue = max(yPrime);
+yNorm = yPrime/maxValue;
+subplot(2,2,4);
+stem(n,yNorm,".")
